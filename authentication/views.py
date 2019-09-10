e9902bf7169427ee5d14ec8fa6f5c47962be7b2c
Fabian Schilf <f.schilf94@gmail.com>
2019-09-11 00:39:04 +0200

    created JWT auth

---
+++ authentication/views.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class GenerateToken(APIView):
+    pass
+
+
+class RefreshToken(APIView):
+    pass
