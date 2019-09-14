d6e3e5d83ec6d50a197ae64a85969ba0c39d66b4
pr0grammr <f.schilf04@gmail.com>
2019-09-14 02:05:42 +0200

    implemented __str__ methods in models

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.text
+
--- users/models.py
+++ users/models.py
@@ -16,6 +16,9 @@ class Interest(models.Model):
     name = models.CharField(max_length=255, null=False)
     value = models.CharField(max_length=255, null=False)
 
+    def __str__(self):
+        return self.value
+
 
 class User(AbstractUser):
     class Meta:
