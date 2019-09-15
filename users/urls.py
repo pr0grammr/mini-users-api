0f0e34160ade5d466b95ba0f9a9d48b66fc7aa18
pr0grammr <f.schilf04@gmail.com>
2019-09-15 15:21:32 +0200

    added view for all users

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    #path('', views.UserListView.as_view(), name='user_list'),
+    path('', views.UsersView.as_view(), name='user_list'),
     path('<int:pk>/', views.RetrieveUpdateDeleteUserView.as_view(), name='user_details'),
     path('<int:pk>/posts/', views.RetrieveUserPostsView.as_view(), name='user_posts')
 ]
